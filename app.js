c485312cb65444f921a17081db1e107957989450
= <rukshanlancer@gmail.com>
2022-11-30 22:26:44 +0800

    login, register level one security

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -10,6 +11,15 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+
+mongoose.connect('mongodb://localhost:27017/userDB', { useNewUrlParser: true });
+
+const userSchema = {
+    email: String,
+    password: String
+}
+
+const User = new mongoose.model('User', userSchema);
 
 app.get('/', function (req, res) {
     res.render('home');
@@ -22,6 +32,38 @@ app.get('/login', function (req, res) {
 app.get('/register', function (req, res) {
     res.render('register');
 });
+
+app.post('/register', function (req, res) {
+    const newUser = new User({
+        email: req.body.email,
+        password: req.body.password
+    });
+    newUser.save(function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('secrets');
+        }
+    });
+});
+
+app.post('/login', function (req, res) {
+    const username = req.body.email;
+    const password = req.body.password;
+
+    User.findOne({ email: username }, function (err, foundUser) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUser) {
+                if (foundUser.password === password) {
+                    res.render('secrets');
+                }
+            }
+        }
+    });
+});
+
 
 app.listen(3000, function () {
     console.log("Sever started on port 3000");
