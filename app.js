f1f74bb5bfc19be71d5544a177d5c09b64f07ee9
= <rukshanlancer@gmail.com>
2022-12-15 23:15:55 +0800

    hash encryption md5

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+// const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 
 const app = express();
 
@@ -21,7 +22,7 @@ const userSchema = new mongoose.Schema({
 });
 // encrypt part
 
-userSchema.plugin(encrypt, { secret: process.env.API_KEY, encryptedFields: ["password"] });
+// userSchema.plugin(encrypt, { secret: process.env.API_KEY, encryptedFields: ["password"] });
 
 const User = new mongoose.model('User', userSchema);
 
@@ -40,7 +41,7 @@ app.get('/register', function (req, res) {
 app.post('/register', function (req, res) {
     const newUser = new User({
         email: req.body.email,
-        password: req.body.password
+        password: md5(req.body.password)
     });
     newUser.save(function (err) {
         if (err) {
@@ -53,7 +54,7 @@ app.post('/register', function (req, res) {
 
 app.post('/login', function (req, res) {
     const username = req.body.email;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({ email: username }, function (err, foundUser) {
         if (err) {
