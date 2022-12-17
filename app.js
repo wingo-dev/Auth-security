ec389a61088652d6d353590a223b86a436eea868
= <rukshanlancer@gmail.com>
2022-12-17 12:27:23 +0800

    fixed bad request and logout issue, should not use email as a variable

--- app.js
+++ app.js
@@ -6,10 +6,7 @@ const mongoose = require('mongoose');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocalMongoose = require('passport-local-mongoose');
-// const encrypt = require('mongoose-encryption');
-// const md5 = require('md5');
-// const bcrypt = require('bcrypt');
-// const salt = 10;
+
 const app = express();
 
 app.use(express.static('public'));
@@ -29,6 +26,7 @@ app.use(passport.session());
 
 mongoose.connect('mongodb://localhost:27017/userDB', { useNewUrlParser: true });
 
+
 const userSchema = new mongoose.Schema({
     email: String,
     password: String
@@ -38,7 +36,6 @@ userSchema.plugin(passportLocalMongoose);
 
 // encrypt part
 
-// userSchema.plugin(encrypt, { secret: process.env.API_KEY, encryptedFields: ["password"] });
 
 const User = new mongoose.model('User', userSchema);
 
@@ -46,6 +43,8 @@ passport.use(User.createStrategy());
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
+
 
 app.get('/', function (req, res) {
     res.render('home');
@@ -67,58 +66,32 @@ app.get('/secrets', function (req, res) {
     }
 })
 
-app.get('logout', function (req, res) {
-    req.logout();
-    res.redirect('/');
-})
+app.get("/logout", (req, res) => {
+    req.logout(req.user, err => {
+        if (err) return next(err);
+        res.redirect("/");
+    });
+});
 
 app.post('/register', function (req, res) {
-    // bcrypt.hash(req.body.password, salt, function (err, hash) {
-    //     // Store hash in your password DB.
-    //     const newUser = new User({
-    //         email: req.body.email,
-    //         password: hash
-    //     });
-    //     newUser.save(function (err) {
-    //         if (err) {
-    //             console.log(err);
-    //         } else {
-    //             res.render('secrets');
-    //         }
-    //     });
-    // });
-    User.register({ username: req.body.email }, req.body.password, function (err, user) {
+    User.register({ username: req.body.username, active: false }, req.body.password, function (err, user) {
         if (err) {
             console.log(err);
             res.redirect('/register');
+
         } else {
+            console.log('123123123');
             passport.authenticate("local")(req, res, function () {
+                console.log("ugnabuna");
                 res.redirect("/secrets");
-            })
+            });
         }
     });
 });
 
 app.post('/login', function (req, res) {
-    // const username = req.body.email;
-    // const password = req.body.password;
-
-    // User.findOne({ email: username }, function (err, foundUser) {
-    //     if (err) {
-    //         console.log(err);
-    //     } else {
-    //         if (foundUser) {
-    //             bcrypt.compare(password, foundUser.password, function (err, result) {
-    //                 // result == true
-    //                 if (result === true) {
-    //                     res.render('secrets');
-    //                 }
-    //             });
-    //         }
-    //     }
-    // });
     const user = new User({
-        username: req.body.email,
+        username: req.body.username,
         password: req.body.password
     });
 
